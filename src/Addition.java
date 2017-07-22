4e8c0f49101f876a960e28a97fcaa678b161c7e7
sneha3188 <sneha.parate31@gmail.com>
2017-07-22 19:11:38 +0530

    Assignment1.1

---
+++ src/Addition.java
@@ -0,0 +1,16 @@
+
+//Program to add two number  and print the output
+
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declare 3 variables
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = num1 + num2;
+		 System.out.println("Addition of "+num1+" and "+num2+" is "+num3);
+	}
+
+}
